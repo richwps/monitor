01203dc3f9d3d5ad8ab226c5e8c891f811685908
Fruchuxs <Fruchuxs@gmail.com>
2014-06-02 12:21:06 +0200

    Initital Commit

---
+++ src/main/java/de/hsosnabrueck/ecs/richwps/wpsmonitor/Application.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.hsosnabrueck.ecs.richwps.wpsmonitor;
+
+/**
+ *
+ * @author FloH
+ */
+public class Application {
+    public static void main(String[] args) {
+        new Application().run();
+    }
+    
+    public Application() {
+        
+    }
+    
+    public void run() {
+        
+    }
+}
