e921baa6e899ab93242bd9fd2817f7a502d5349e
Fruchuxs <Fruchuxs@gmail.com>
2014-06-21 22:55:14 +0200

    QosDao-Tests added

--- src/test/java/de/hsosnabrueck/ecs/richwps/wpsmonitor/data/dataaccess/defaultimpl/QosDaoTest.java
+++ src/test/java/de/hsosnabrueck/ecs/richwps/wpsmonitor/data/dataaccess/defaultimpl/QosDaoTest.java
@@ -33,7 +33,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
