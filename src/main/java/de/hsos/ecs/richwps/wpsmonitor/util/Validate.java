5f4c7c6a8efd0921273a7299814abc040353eaba
Fruchuxs <Fruchuxs@gmail.com>
2014-08-13 20:58:00 +0200

    min,max, notEmpty string validation methods added

--- src/main/java/de/hsos/ecs/richwps/wpsmonitor/util/Validate.java
+++ src/main/java/de/hsos/ecs/richwps/wpsmonitor/util/Validate.java
@@ -29,11 +29,55 @@ public class Validate {
      * @param name Outputname for exception message
      * @return T
      */
-    public static <T> T notNull(T var, String name) {
+    public static <T> T notNull(final T var, final String name) {
         if (var == null) {
             throw new IllegalArgumentException(name + " was null.");
         }
 
+        return var;
+    }
+    
+    /**
+     * Checks the maximal length of a String.
+     * 
+     * @param var String instance to validate
+     * @param count max allowed characters
+     * @return var
+     */
+    public static String max(final String var, final Long count) {
+        if(var.length() > count) {
+            throw new IllegalArgumentException("The given String instance is longer as allowed (" + count + ')');
+        }
+        
+        return var;
+    }
+    
+    /**
+     * Checks the minimal length of a String.
+     * 
+     * @param var String instance to validate
+     * @param count minimal allowed characters
+     * @return var
+     */
+    public static String min(final String var, final Long count) {
+        if(var.length() < count) {
+            throw new IllegalArgumentException("The given String instance is shorter as allowed (" + count + ')');
+        }
+        
+        return var;
+    }
+    
+    /**
+     * Checks if the given String is not Empty
+     * 
+     * @param var String to validate
+     * @return var
+     */
+    public static String notEmpty(final String var) {
+        if(var == null || var.isEmpty()) {
+            throw new IllegalArgumentException("The given String instance is empty");
+        }
+        
         return var;
     }
 
@@ -43,7 +87,7 @@ public class Validate {
      * @param t
      * @param name Outputname for exception message
      */
-    public static void isTrue(Boolean t, String name) {
+    public static void isTrue(final Boolean t, final String name) {
         notNull(t, name);
 
         if (!t) {
